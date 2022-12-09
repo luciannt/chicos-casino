4d88ee76f594abd1a0d761a4785a5d09e485ef6a
Lucianna Tittle <lucianna.roset@gmail.com>
2022-12-09 00:56:05 -0600

    updates

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import styles from "./styles/Board.module.scss";
 
-const Board = ({ ctx, G, moves }) => {
-  return <div>Board</div>;
+const image_host = "https://raw.githubusercontent.com/luciannt/galaxy-party/main/client/src/assets/cards"
+
+const Board = ({ ctx, G, moves, events }) => {
+  const game = useSelector((state) => state.game);
+  const gameChannel = useSelector((state) => state.connections.game);
+
+  const { code } = useParams();
+
+  useEffect(() => {
+    if (ctx.turn === 1) {
+      if (!game?.players[0]?.active_hand) {
+      }
+    }
+  }, [ctx]);
+
+  const getCardImage = (rank, suit) => {
+    return `${image_host}/${}`
+  }
+
+  return <div className={styles.container}>{
+    game.hand.map(card => (
+      <img src={getCardImage(card.rank, card.suit)} />
+    ))
+  }</div>;
 };
 
 export default Board;
